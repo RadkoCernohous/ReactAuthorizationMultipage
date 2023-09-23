5caa803c2c332914a2254df8ed2c9a30dd4f5064
Your Name <radko.cernohous.jr@seznam.cz>
2023-09-23 11:11:41 +0200

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -58,7 +58,7 @@ const router = createBrowserRouter([
             path: 'new',
             element: <NewEventPage />,
             action: manipulateEventAction,
-            loader:checkAuthLoader
+            loader:checkAuthLoader,
           },
         ],
       },
